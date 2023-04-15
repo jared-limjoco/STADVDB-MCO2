02b2d213a0848eab902ed7ab94d537d3018bdba4
jared-limjoco <limjocojared9@gmail.com>
2023-04-15 10:26:35 +0800

    Update index.js
    stadvdb-mco2.up.railway.app

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => { //function()
-    fetch('stadvdb.up.railway.app/getAll')
+    fetch('stadvdb-mco2.up.railway.app/getAll')
     .then(res => res.json())
     .then(data => loadHTMLTable(data['data']));
 });
@@ -22,7 +22,7 @@ reportBtn.onclick = function(){
     const reportTable = document.querySelector('#report-table');
     reportTable.hidden = false
 
-    fetch('stadvdb.up.railway.app/report')
+    fetch('stadvdb-mco2.up.railway.app/report')
     .then(res => res.json())
     .then(data => loadReport(data['data']));
 }
@@ -31,13 +31,13 @@ searchBtn.onclick = function(){
     const searchNameValue = document.querySelector('#search-name-input').value;
     const searchYearValue = document.querySelector('#search-year-input').value;
 
-    fetch('stadvdb.up.railway.app/search/' + searchNameValue + "/" + searchYearValue)
+    fetch('stadvdb-mco2.up.railway.app/search/' + searchNameValue + "/" + searchYearValue)
     .then(res => res.json())
     .then(data => loadHTMLTable(data['data']));
 }
 
 function deleteRowById(id){
-    fetch('stadvdb.up.railway.app/delete/' + id, {
+    fetch('stadvdb-mco2.up.railway.app/delete/' + id, {
         method: 'DELETE'
     })
     .then(response => response.json())
@@ -58,7 +58,7 @@ function handleEditRow(id){
 updateBtn.onclick = function(){
     const updateRatingeInput = document.querySelector('#update-rating-input');
 
-    fetch('stadvdb.up.railway.app/update', {
+    fetch('stadvdb-mco2.up.railway.app/update', {
         method: 'PATCH',
         headers: {
             'Content-type': 'application/json'
@@ -94,7 +94,7 @@ addBtn.onclick = function (){
     yearInput.value = '';
     ratingInput.value = '';
 
-    fetch('stadvdb.up.railway.app/insert', {
+    fetch('stadvdb-mco2.up.railway.app/insert', {
         headers: {
             'Content-type': 'application/json'
         },
